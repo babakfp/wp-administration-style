05038f46c3ea280e37c0cbfec48b85d6a64b9321
babakfp <44144724+babakfp@users.noreply.github.com>
2023-05-04 06:13:41 -0700

    6.14.0

--- wp-administration-style.php
+++ wp-administration-style.php
@@ -4,24 +4,25 @@ namespace WP_ADMINISTRATION_STYLE;
 defined( 'ABSPATH' ) or die;
 
 /**
- * Plugin Name:           WP Administration Style
- * Version:               6.13.0
- * Description:           Enhances WordPress admin panel design and user experience.
- * Author:                babakfp
- * Author URI:            https://babakfp.ir
- * Tested up to:          6.2.0
- * License:               GPLv3 or later
- * License URI:           https://www.gnu.org/licenses/gpl-3.0.html
- * Text Domain:           wp-administration-style
- * Domain Path:           /languages
- * Tags:                  farsi, farsi font, farsi dashboard, فارسی, فونت فارسی, داشبورد فارسی, فارسی‌سازی
+ * Plugin Name:                       WP Administration Style
+ * Version:                           6.14.0
+ * Description:                       Enhances WordPress admin panel design and user experience.
+ * Author:                            babakfp
+ * Author URI:                        https://babakfp.ir
+ * Tested up to:                      6.2.0
+ * Tested (Elementor) up to:          3.12.2
+ * License:                           GPLv3 or later
+ * License URI:                       https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain:                       wp-administration-style
+ * Domain Path:                       /languages
+ * Tags:                              farsi, farsi font, farsi dashboard, فارسی, فونت فارسی, داشبورد فارسی, فارسی‌سازی
  */
 
 if ( ! class_exists( 'Wp_Administration_Style_Globals' ) )
 {
 	final class Wp_Administration_Style_Globals
 	{
-		public static $version = '6.13.0';
+		public static $version = '6.14.0';
 
 		public static function url() {
 			return plugin_dir_url( __FILE__ );
